cee4c127c35cc2d456902b167671396e5603a898
Champkub999 <91883450+Champkub999@users.noreply.github.com>
2021-10-25 12:55:35 +0700

    Update and rename README.md to xenon.lua

---
+++ xenon.lua
@@ -0,0 +1,16 @@
+_G.Settings = {
+['Name'] = 'ItsBlack',
+['Intro'] = true,
+['Keybind'] = 'RightControl'
+}
+
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/QPehPJ6m", true))()
+local Tab1 = Library:CreateTab("AutoFarm⚔️","http://www.roblox.com/asset/?id=4805639000")
+local Tab1 = Library:CreateTab("Stats❤️","http://www.roblox.com/asset/?id=4805639000")
+local Tab1 = Library:CreateTab("Setting AutoFarm⚙️","http://www.roblox.com/asset/?id=4805639000")
+local Tab1 = Library:CreateTab("Players🧔‍")
+local Tab1 = Library:CreateTab("Teleport🗺️")
+local Tab1 = Library:CreateTab("Misc"🖥)
+local Tab1 = Library:CreateTab("Dunguen🏠")
+local Tab1 = Library:CreateTab("BuyItem💸")
+local Tab1 = Library:CreateTab("Setting⚙︎")
