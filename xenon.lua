ba5c6ed0633e7757669f892c6d990b4f4a203c5d
Champkub999 <91883450+Champkub999@users.noreply.github.com>
2021-10-25 13:15:59 +0700

    Update xenon.lua

--- xenon.lua
+++ xenon.lua
@@ -1,16 +1,16 @@
 _G.Settings = {
-['Name'] = 'ItsBlack',
+['Name'] = 'BlackList',
 ['Intro'] = true,
-['Keybind'] = 'RightControl'
+['Keybind'] = 'G'
 }
 
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/QPehPJ6m", true))()
-local Tab1 = Library:CreateTab("AutoFarm⚔️","http://www.roblox.com/asset/?id=4805639000")
-local Tab1 = Library:CreateTab("Stats❤️","http://www.roblox.com/asset/?id=4805639000")
-local Tab1 = Library:CreateTab("Setting AutoFarm⚙️","http://www.roblox.com/asset/?id=4805639000")
-local Tab1 = Library:CreateTab("Players🧔‍")
-local Tab1 = Library:CreateTab("Teleport🗺️")
-local Tab1 = Library:CreateTab("Misc"🖥)
-local Tab1 = Library:CreateTab("Dunguen🏠")
-local Tab1 = Library:CreateTab("BuyItem💸")
-local Tab1 = Library:CreateTab("Setting⚙︎")
+local Tab1 = Library:CreateTab("AutoFarm","http://www.roblox.com/asset/?id=4805639000")
+local Tab1 = Library:CreateTab("Stats","http://www.roblox.com/asset/?id=4805639000")
+local Tab1 = Library:CreateTab("Setting AutoFarm","http://www.roblox.com/asset/?id=4805639000")
+local Tab1 = Library:CreateTab("️Players")
+local Tab1 = Library:CreateTab("Teleport")
+local Tab1 = Library:CreateTab("Mics")
+local Tab1 = Library:CreateTab("Dunguen")
+local Tab1 = Library:CreateTab("BuyItem")
+local Tab1 = Library:CreateTab("Setting")
